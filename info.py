a09c7507a6863428b45ac3c20d8540ab304e2fa3
botwalain <167398125+botwalain@users.noreply.github.com>
2024-11-21 21:46:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,7 +107,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "https://daily-deloris-movbotbyibm-fac99e76.koyeb.app/")
+URL = environ.get("FQDN", "https://gothic-aloysia-movbotbyibm-90f1fdcc.koyeb.app/")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
